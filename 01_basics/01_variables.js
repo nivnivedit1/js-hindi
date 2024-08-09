94b3d41e9b7f77188b93667205529a0fa95c485b
nivnivedit1 <164485166+nivnivedit1@users.noreply.github.com>
2024-08-09 08:26:51 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "nivi@1234"6
+var acoountPassword = "12345"
+accountCity = "Bidar"
+
+
+// accountId = 2 // not allowed
+console.log(accountId);
+
+accountEmail = "ni123c.com"
