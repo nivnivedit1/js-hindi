4a3d41e89888010fa6868792241ae37a1b766413
nivnivedit1 <164485166+nivnivedit1@users.noreply.github.com>
2024-08-09 08:25:28 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+
+console.log("nivi")
+
+// number => 2 to power 55 
+//bigint
+//string => ""
+// boolean => true/false 
+// null => its a standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0 
+
+// ***************operations********************************
+
+let value = 3 
+let negValue = -value
+console.log(negValue);
+// addition,substraction,multi,division(/),power(**)
+
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log("2" > 1);
+
