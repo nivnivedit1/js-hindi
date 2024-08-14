e56e1b096ba530619188f88749e6771bc26177dc
nivnivedit1 <164485166+nivnivedit1@users.noreply.github.com>
2024-08-14 10:33:09 +0000

    completed

---
+++ 01_basics/05_nums_and_math.js
@@ -0,0 +1,18 @@
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ****************MATHS******************************************* ITS A labrary in js
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+
